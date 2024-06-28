004f8db567dba723624dde34a29c5fc8556989bd
ccxiaoyong <ubuntu@localhost.localdomain>
2024-06-28 23:11:03 +0800

    test

--- syvector/test.cpp
+++ syvector/test.cpp
@@ -6,14 +6,8 @@ using namespace std;
 int main() {
     vector<int> v;
     syvector<int> syv(6,6);
-    // syv.push_back(2);
-    // syv.push_back(3);
-    // syv.push_back(4);
-    // syv.push_back(5);
-    // syv.push_back(6);
-
     for(int i = 0; i < 6; ++i){
-        // syv.push_back(i);
+        syv.push_back(i);
         cout << syv.capacity() << " " << syv[i] << endl;
     }
     return 0;
